56420055544951cc609cafe67685081b193a410e
Darian Moody <mail@djm.org.uk>
2011-07-19 18:15:51 +0100

    Adding translation support to models.

--- kanban/models.py
+++ kanban/models.py
@@ -1,17 +1,16 @@
 from django.db import models
-from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext_lazy as _
 
 ###
-# Abstract Base classes
+# Abstract Base models.
 ###
 
 class TimestampModel(models.Model):
     """A short mixin abstract class for adding 
     update and creation timestamp fields.
     """
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(_('created'), auto_now_add=True)
+    modified = models.DateTimeField(_('modified'), auto_now_add=True)
 
     class Meta:
         abstract = True
@@ -21,18 +20,21 @@ class ActiveModel(models.Model):
     """A mixin abstract class to provide functionality
     for marking specific rows as active or inactive.
     """
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(_('is active?'), default=True)
 
     class Meta:
         abstract = True
 
+###
+# Core models.
+###
 
 class Category(models.Model):
     """Allows simple categorisation of the boards, 
     whether that be by client, location, whatever.
     """
-    title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
+    title = models.CharField(_('title'), max_length=100, unique=True)
+    slug = models.SlugField(_('slug'), max_length=100, unique=True)
 
     class Meta:
         ordering = ['title']
@@ -50,12 +52,14 @@ class Board(TimestampModel, ActiveModel):
     chart and all of its attributes.
     """
     #TODO: editing permissions, needs groups and users.
-    creator = models.ForeignKey('auth.User', related_name='boards')
-    category = models.ForeignKey('kanban.Category', related_name='boards')
-    title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    tagline = models.CharField(max_length=255, blank=True)
-    description = models.TextField(blank=True)
+    creator = models.ForeignKey('auth.User', verbose_name=_('creator'),
+                                related_name='boards')
+    category = models.ForeignKey('kanban.Category', verbose_name=_('category'),
+                                related_name='boards')
+    title = models.CharField(_('title'), max_length=100, unique=True)
+    slug = models.SlugField(_('slug'), max_length=100, unique=True)
+    tagline = models.CharField(_('tagline'), max_length=255, blank=True)
+    description = models.TextField(_('description'), blank=True)
 
     class Meta:
         ordering = ['-created']
@@ -74,11 +78,12 @@ class State(models.Model):
     boards which do different tasks.
     """
     #TODO: editing permissions, needs groups and users.
-    board = models.ForeignKey('kanban.Board', related_name='columns')
-    title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    tagline = models.CharField(max_length=255, blank=True)
-    order = models.PositiveIntegerField()
+    board = models.ForeignKey('kanban.Board', verbose_name=_('board'), 
+                              related_name='columns')
+    title = models.CharField(_('title'), max_length=100, unique=True)
+    slug = models.SlugField(_('slug'), max_length=100, unique=True)
+    tagline = models.CharField(_('tagline'), max_length=255, blank=True)
+    order = models.PositiveIntegerField(_('order'))
 
     class Meta:
         ordering = ['order']
@@ -90,7 +95,8 @@ class Card(models.Model):
     """Represents a 'kanban card', kanban cards sit
     in a given state on a given board and model a single task.
     """
-    state = models.ForeignKey('kanban.State', related_name='cards')
-    title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(max_length=100, unique=True)
-    description = models.TextField(blank=True)
+    state = models.ForeignKey('kanban.State', verbose_name=_('state'), 
+                              related_name='cards')
+    title = models.CharField(_('title'), max_length=100, unique=True)
+    slug = models.SlugField(_('slug'), max_length=100, unique=True)
+    description = models.TextField(_('description'), blank=True)
